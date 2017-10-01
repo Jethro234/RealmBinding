3a31bf7d37a64d6a9fdc23dcd7bf88e35041bc05
Jethro234 <jimmybuxton@hotmail.com>
2017-10-01 15:43:56 +0100

    Changed ic_launcher background colour.

--- app/src/main/java/com/example/james/realmbinding/scan/OcrDetectorProcessor.java
+++ app/src/main/java/com/example/james/realmbinding/scan/OcrDetectorProcessor.java
@@ -25,7 +25,6 @@ import com.google.android.gms.vision.text.TextBlock;
 /**
  * A very simple Processor which gets detected TextBlocks and adds them to the overlay
  * as OcrGraphics.
- * TODO: Make this implement Detector.Processor<TextBlock> and add text to the GraphicOverlay
  */
 public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
 
@@ -34,8 +33,6 @@ public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
     OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
         mGraphicOverlay = ocrGraphicOverlay;
     }
-
-    // TODO:  Once this implements Detector.Processor<TextBlock>, implement the abstract methods.
 
     @Override
     public void release() {
