27e701ef77872e9fc9c2239437b5f0a725c12317
Jethro234 <jimmybuxton@hotmail.com>
2017-10-08 18:53:42 +0100

    Added new options to navigation panel.

--- app/src/main/java/com/example/james/realmbinding/view/activites/MainActivity.java
+++ app/src/main/java/com/example/james/realmbinding/view/activites/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.james.realmbinding.view.activites;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.james.realmbinding.R;
@@ -21,39 +28,86 @@ import com.example.james.realmbinding.workout.RecordWOD;
 
 import org.joda.time.DateTime;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.realm.RealmConfiguration;
 
-import static com.example.james.realmbinding.utils.Constants.*;
+import static com.example.james.realmbinding.utils.Constants.SCANNED_EXERCISE;
+import static com.example.james.realmbinding.utils.Constants.SCANNED_TIME;
+import static com.example.james.realmbinding.utils.Constants.SCAN_WOD_BUNDLE_KEY;
 
-/**
- * Project: Workout Logger App
- * Created by James on 06-Aug-16.
- */
-public class MainActivity extends AppCompatActivity implements RealmCallback {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, RealmCallback {
 
-    Context context;
+    private Context context;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         context = this;
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWod();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    public void onScanWodClick(View view) {
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void scanWod() {
         Intent i = new Intent(context, OcrCaptureActivity.class);
         startActivityForResult(i, Constants.SCAN_WOD);
     }
 
-    public void onRecordWodClick(View view){
+    public void recordWod(){
         Intent i = new Intent(context, RecordWOD.class);
         startActivity(i);
     }
 
-    public void onViewProgress(View view) {
+    public void viewProgress() {
         Intent i = new Intent(context, ViewProgress.class);
         startActivity(i);
     }
@@ -100,6 +154,26 @@ public class MainActivity extends AppCompatActivity implements RealmCallback {
     public void Failure(Throwable error, String errorMessage) {
         Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
         error.printStackTrace();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_record_wod:
+                recordWod();
+                break;
+            case R.id.nav_view_progress:
+                viewProgress();
+                break;
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
