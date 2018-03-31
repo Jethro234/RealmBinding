01a568eeaa2d8f7d484d0e7876a900b09ae92199
Jethro234 <jimmybuxton@hotmail.com>
2018-03-31 13:13:00 +0100

    Application class no longer extends DaggerApplication.

--- app/src/main/java/com/example/james/wodrecordapp/ControlApplication.java
+++ app/src/main/java/com/example/james/wodrecordapp/ControlApplication.java
@@ -1,9 +1,17 @@
 package com.example.james.wodrecordapp;
 
+import android.app.Activity;
+import android.app.Application;
+import android.app.Fragment;
+
 import com.example.james.wodrecordapp.di.component.DaggerApplicationComponent;
 
+import javax.inject.Inject;
+
 import dagger.android.AndroidInjector;
-import dagger.android.DaggerApplication;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import dagger.android.HasFragmentInjector;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -11,11 +19,15 @@ import io.realm.RealmConfiguration;
  * Created by jimmy on 08/10/2017.
  */
 
-public class ControlApplication extends DaggerApplication {
+public class ControlApplication extends Application implements HasActivityInjector, HasFragmentInjector {
+
+    @Inject DispatchingAndroidInjector<Activity> activityInjector;
+    @Inject DispatchingAndroidInjector<Fragment> fragmentInjector;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        DaggerApplicationComponent.builder().application(this).build().inject(this);
 
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder()
@@ -25,7 +37,12 @@ public class ControlApplication extends DaggerApplication {
     }
 
     @Override
-    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return DaggerApplicationComponent.builder().application(this).build();
+    public AndroidInjector<Fragment> fragmentInjector() {
+        return fragmentInjector;
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return activityInjector;
     }
 }
--- app/src/main/java/com/example/james/wodrecordapp/di/module/ApplicationModule.java
+++ app/src/main/java/com/example/james/wodrecordapp/di/module/ApplicationModule.java
@@ -1,8 +1,11 @@
 package com.example.james.wodrecordapp.di.module;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.example.james.wodrecordapp.ControlApplication;
+
+import javax.inject.Singleton;
 
 import dagger.Binds;
 import dagger.Module;
@@ -13,9 +16,19 @@ import dagger.Module;
 
 @Module
 public abstract class ApplicationModule {
+    @Binds
+    @Singleton
+    /*
+     * Singleton annotation isn't necessary since Application instance is unique but is here for
+     * convention. In general, providing Activity, Fragment, BroadcastReceiver, etc does not require
+     * them to be scoped since they are the components being injected and their instance is unique.
+     *
+     * However, having a scope annotation makes the module easier to read. We wouldn't have to look
+     * at what is being provided in order to understand its scope.
+     */
+    abstract Application application(ControlApplication appVar);
+
     //expose Application as an injectable context
     @Binds
     abstract Context bindContext(ControlApplication application);
-
-
 }
